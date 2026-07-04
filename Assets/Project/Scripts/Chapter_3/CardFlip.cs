28092573a3d2398c75f8e08ba2e5e0ef2de45a89
frozengk <1254310291@qq.com>
2026-07-04 23:20:53 +0800

    <feat> chapter3新增交互

---
+++ Assets/Project/Scripts/Chapter_3/CardFlip.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CardFlip : MonoBehaviour
+{
+    public GameObject frontSide;   
+    public GameObject backSide;    
+    private bool isFront = true;   // 当前是否正面
+
+    void Start()
+    {
+        frontSide.SetActive(true);
+        backSide.SetActive(false);
+        isFront = true;
+    }
+
+
+    public void Flip()
+    {
+        isFront = !isFront;
+        frontSide.SetActive(isFront);
+        backSide.SetActive(!isFront);
+    }
+}
