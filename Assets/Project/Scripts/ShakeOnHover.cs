202cc6e87e1f402d85385a1cbb43bd4b70707361
frozengk <1254310291@qq.com>
2026-07-04 12:48:58 +0800

    <feat> 新增场景3，模块交互，鼠标抖动脚本， <fix> 字体渲染

---
+++ Assets/Project/Scripts/ShakeOnHover.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+
+public class ShakeOnHover : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+{
+    public float shakeAmount = 5f;          // 抖动幅度
+    public float shakeDuration = 0.2f;      // 抖动时间
+    private RectTransform rectTransform;
+    private Vector3 originalPosition;
+    private Coroutine shakeCoroutine;
+
+    void Start()
+    {
+        rectTransform = GetComponent<RectTransform>();
+        originalPosition = rectTransform.localPosition;
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        if (shakeCoroutine != null)
+            StopCoroutine(shakeCoroutine);
+        shakeCoroutine = StartCoroutine(Shake());
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        if (shakeCoroutine != null)
+        {
+            StopCoroutine(shakeCoroutine);
+            shakeCoroutine = null;
+        }
+        rectTransform.localPosition = originalPosition;
+    }
+
+    IEnumerator Shake()
+    {
+        float elapsed = 0f;
+        while (elapsed < shakeDuration)
+        {
+            float x = Random.Range(-shakeAmount, shakeAmount);
+            float y = Random.Range(-shakeAmount, shakeAmount);
+            rectTransform.localPosition = originalPosition + new Vector3(x, y, 0);
+            elapsed += Time.deltaTime;
+            yield return null;
+        }
+        rectTransform.localPosition = originalPosition;
+    }
+}
